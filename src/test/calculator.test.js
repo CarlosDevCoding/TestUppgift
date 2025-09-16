372502d21da64ec6772f775012e0975981eef2b1
Carlos Ibanez <carlos.ibanez@live.se>
2025-09-16 23:07:26 +0200

    Lagt till och testat alla 4 räknesätt med stora nummer

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -3,5 +3,19 @@ import { createCalculator } from "../lib/Calculator";
 
 test("Adderar två tal korrekt", () => {
   const calculator = createCalculator();
-  expect(calculator.add(2, 3)).toBe(5);
+  expect(calculator.add(123, 456)).toBe(579);
+}); 
+
+test("Subtraherar två tal korrekt", () => {  
+  const calculator = createCalculator();
+  expect(calculator.subtract(987, 654)).toBe(333);
+});
+
+test("Multiplicerar två tal korrekt", () => {  
+  const calculator = createCalculator();
+  expect(calculator.multiply(123, 456)).toBe(56088);
+}); 
+test("Dividerar två tal korrekt", () => {  
+  const calculator = createCalculator();
+  expect(calculator.divide(987, 654)).toBe(1.5091743119266055);
 }); 
